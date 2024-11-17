d42fa6ce27fd04840743d205568ae6edf66d5083
Dan-Popescu <popescu_dan@outlook.com>
2024-11-17 20:49:39 +0100

    Updated file reading

--- main.c
+++ main.c
@@ -71,19 +71,74 @@ int main(int argc, char ** argv) {
             free_global_scope(global_scope);
         }
     }else if(argc == 2){
-        // check if file input is in correct format and file exists
-        // get second argument which corresponds to first user input passed to the executable
+        // Check if file input is correct
         char * filepath = argv[1];
-//        printf("\n filepath string provided as argument to the program : %s", filepath);
         unsigned short file_input_check = validate_file_input(filepath);
         if(file_input_check == WRONG_MAIN_INPUT_FILE_EXTENSION){
             printf("\nFile input must have a .zl extension.");
         }else if(file_input_check == FILE_DOES_NOT_EXIST){
             printf("\nFile does not exist");
-        }else if(file_input_check == VALID_INPUT) printf("\n File input OK");
-        printf("\n file input check is : %hu", file_input_check);
+        }else if(file_input_check == VALID_INPUT){
+//            printf("\nFile input OK\n");
 
+            // File processing logic
+            FILE *file = fopen(filepath, "r");
+            if(file == NULL){
+                printf("\nError opening file.\n");
+                return 1;
+            }
 
+            GLOBAL_SCOPE * global_scope = init_global_scope(20);
+
+            char curr_char = 0;
+            unsigned short CHUNK_SIZE = 500;
+            unsigned short capacity = CHUNK_SIZE;
+            char * file_content = calloc(capacity, sizeof(char));
+            unsigned short length = 0;
+            curr_char = fgetc(file);
+
+            while( curr_char != EOF){
+                if(length  + 1 > capacity){
+                    capacity += CHUNK_SIZE;
+                    char * temp = realloc(file_content, capacity);
+                    if(temp == NULL){
+                        fprintf(stderr, "Memory reallocation failed for file content.");
+                        free(file_content);
+                        fclose(file);
+                        exit(EXIT_FAILURE);
+                    }
+                    file_content = temp;
+                }
+
+                // append the character and update the length
+                file_content[length] = curr_char;
+                length++;
+
+                curr_char = fgetc(file);
+            }
+
+            file_content[length] = '\0';
+
+            Lexer * lexer = create_lexer(file_content);
+            Parser * parser = create_parser(lexer);
+            Interpreter * interpreter = create_interpreter(parser, global_scope);
+
+            ASTNode * tree = statements_list(parser);
+
+            int * res_val = interpret(interpreter, tree);
+
+            // Free memory for interpreter, parser, and lexer
+            free_interpreter(interpreter);
+            free(res_val);
+            interpreter = NULL;
+            free_node(tree);
+            tree = NULL;
+
+            fclose(file);
+            if(global_scope != NULL){
+                free_global_scope(global_scope);
+            }
+        }
 
     }else if(argc > 2){
         printf("zlang command accepts no input or a filepath string as input. Either execute zlang without "
@@ -95,7 +150,6 @@ int main(int argc, char ** argv) {
 
 unsigned short validate_file_input(char * filepath){
     char * file_extension = strrchr(filepath, '.');
-    printf("\n file extension checked in validate_field_input : %s", file_extension);
     if(file_extension == NULL || strcmp(file_extension, ".zl") != 0){
         return WRONG_MAIN_INPUT_FILE_EXTENSION;
     }
